a87ffde0ca091ccc2aadf4b3ae0d2289a917c562
billy1 <>
2023-08-01 11:45:35 +0800

    二分（折半）查找代码片段

---
+++ find/types.go
@@ -0,0 +1,31 @@
+package find
+
+// Signed 有符合整数
+type Signed interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64
+}
+
+// Unsigned 无符号整数
+type Unsigned interface {
+	~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64
+}
+
+// Integer 整数
+type Integer interface {
+	Signed | Unsigned
+}
+
+// Float 浮点数
+type Float interface {
+	~float32 | ~float64
+}
+
+// Number 数字
+type Number interface {
+	Integer | Float
+}
+
+// Ordered 数字或字符串
+type Ordered interface {
+	Number | ~string
+}
